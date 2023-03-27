15d8115db0af0dc5e7e3227e8c4d3a0fb60c1fbb
toyozaki <61676668+toyozaki@users.noreply.github.com>
2023-03-27 19:53:58 +0900

    Fix process to generate taskdef.json

--- generate-taskdef.sh
+++ generate-taskdef.sh
@@ -6,3 +6,4 @@ sed -i -e "s#<EXECUTION_ROLE_ARN>#${EXECUTION_ROLE_ARN}#" taskdef.json
 sed -i -e "s#<TASK_ROLE_ARN>#${TASK_ROLE_ARN}#" taskdef.json
 sed -i -e "s#<CONTAINER_NAME>#${CONTAINER_NAME}#" taskdef.json
 sed -i -e "s#<LOGGROUP_NAME>#${LOGGROUP_NAME}#" taskdef.json
+sed -i -e "s#<AWS_REGION>#${AWS_REGION}#" taskdef.json
